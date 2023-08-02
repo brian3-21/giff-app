ddc2ed2f15bfbd0452c143dac5b98cca0d36e610
Briannn <brianperezdota@gmail.com>
2023-08-02 16:54:09 -0400

    create main-page-component for gifs

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PagesComponent } from './pages/pages.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PagesComponent
+  ],
   imports: [
     CommonModule
   ],
---
+++ src/app/gifs/pages/pages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gif-home-page',
+  templateUrl: './home-pages.component.html',
+  styleUrls: ['./home-pages.component.css']
+})
+export class HomePageComponent {
+
+}
